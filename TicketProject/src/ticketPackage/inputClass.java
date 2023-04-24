0b8916fb55c9e831fa3ac380d1ffd97c32d507c1
WSU <woo4091009@naver.com>
2023-04-24 17:32:34 +0900

    입력 이외의 코드를 삭제 또는 이동

--- TicketProject/src/ticketPackage/inputClass.java
+++ TicketProject/src/ticketPackage/inputClass.java
@@ -3,54 +3,18 @@ package ticketPackage;
 import java.util.Scanner;
 
 public class inputClass {
+	OutputClass opc = new OutputClass();
 	Scanner scanner = new Scanner(System.in);
-	private static int ticketSelect;
-	public static int getTicketSelect() {
-		return ticketSelect;
-		
-	}
-	public static void setTicketSelect(int ticketSelect) {
-		inputClass.ticketSelect = ticketSelect;
-	}
 
-	public static long getCustomerIDNumber() {
-		return customerIDNumber;
-	}
-
-	public static void setCustomerIDNumber(long customerIDNumber) {
-		inputClass.customerIDNumber = customerIDNumber;
-	}
-
-	public static int getOrderCount() {
-		return orderCount;
-	}
-
-	public static void setOrderCount(int orderCount) {
-		inputClass.orderCount = orderCount;
-	}
-
-	public static int getDiscountSelect() {
-		return discountSelect;
-	}
-
-	public static void setDiscountSelect(int discountSelect) {
-		inputClass.discountSelect = discountSelect;
-	}
-
-	private static long customerIDNumber;
-	private static int orderCount;
-	private static int discountSelect;
-	
-
-	public int inputTicketSelect() {		
+	public int inputTicketSelect() {
 		int ticketSelect = 0;
-		System.out.println("권종을 선택하세요.\n1. 주간권\n2. 야간권\n");
+		opc.PrintinputTicketSelect();
 		while (true) {
 			ticketSelect = scanner.nextInt();
 			if (ticketSelect == 1 || ticketSelect == 2) {
 				break;
 			} else {
-				OutputClass.errorMessagePrint();
+				opc.errorMessagePrint();
 			}
 		}
 		return ticketSelect;
@@ -58,14 +22,14 @@ public class inputClass {
 
 	public long inputCustomerIDNumber() {
 		long customerIDNumber = 0L;
-		System.out.println("주민등록번호를 입력하세요.\n");
+		opc.PrintinputCustomerIDNumber();
 		while (true) {
 			customerIDNumber = scanner.nextLong();
 			if ((customerIDNumber >= TicketConstValueClass.FULL_DIGIT_MIN
 					&& customerIDNumber < TicketConstValueClass.FULL_DIGIT)) {
 				break;
 			} else {
-				OutputClass.errorMessagePrint();
+				opc.errorMessagePrint();
 			}
 		}
 		return customerIDNumber;
@@ -73,14 +37,13 @@ public class inputClass {
 
 	public int inputOrderCount() {
 		int orderCount = 0;
-
-		System.out.println("몇개를 주문하시겠습니까(최대 10개)\n");
+		opc.PrintinputOrderCount();
 		while (true) {
 			orderCount = scanner.nextInt();
 			if ((orderCount <= TicketConstValueClass.MAX_COUNT) && (orderCount >= TicketConstValueClass.MIN_COUNT)) {
 				break;
 			} else {
-				OutputClass.errorMessagePrint();
+				opc.errorMessagePrint();
 			}
 		}
 		return orderCount;
@@ -88,23 +51,29 @@ public class inputClass {
 
 	public int inputDiscountSelect() {
 		int discountSelect = 0;
-		System.out.println("우대사항을 선택하세요.\n1. 없음\n2. 장애인\n3. 국가유공자\n4. 다자녀\n5. 임산부\n");
+		opc.PrintinputDiscountSelect();
 		while (true) {
 			discountSelect = scanner.nextInt();
 			if (discountSelect >= 1 && discountSelect <= 5) {
 				break;
 			} else {
-				OutputClass.errorMessagePrint();
+				opc.errorMessagePrint();
 			}
 		}
 		return discountSelect;
 	}
 
-	public void inputData() {
-		ticketSelect = inputTicketSelect();
-		customerIDNumber = inputCustomerIDNumber();
-		orderCount = inputOrderCount();
-		discountSelect = inputDiscountSelect();
+	public int orderContinue() {
+		int continueSelect;
+		opc.PrintOrderContinue();
+		while (true) {
+			continueSelect = scanner.nextInt();
+			if (continueSelect == 1 || continueSelect == 2) {
+				break;
+			} else {
+				opc.errorMessagePrint();
+			}
+		}
+		return continueSelect;
 	}
-
 }
