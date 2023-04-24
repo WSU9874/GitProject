1c663907af84335b4efdaa9f15647dc97bad58a9
WSU <woo4091009@naver.com>
2023-04-24 17:36:24 +0900

    간단하게 표시

--- TicketProject/src/ticketPackage/TicketConstValueClass.java
+++ TicketProject/src/ticketPackage/TicketConstValueClass.java
@@ -1,53 +1,27 @@
 package ticketPackage;
 
-
 public class TicketConstValueClass {
-	
-		public static final int BABY_PRICE= 0;
-		public static final int ADULT_DAY_PRICE = 56000;
-		public static final int ADULT_NIGHT_PRICE = 46000;
-		public static final int TEEN_DAY_PRICE = 47000;
-		public static final int TEEN_NIGHT_PRICE = 46000;
-		public static final int CHILD_DAY_PRICE = 44000;
-		public static final int CHILD_NIGHT_PRICE = 37000;
-		public static final int OLD_DAY_PRICE = 44000;
-		public static final int OLD_NIGHT_PRICE = 37000;
+
+	public static final int BABY_PRICE = 0, ADULT_DAY_PRICE = 56000, ADULT_NIGHT_PRICE = 46000, 
+			TEEN_DAY_PRICE = 47000, TEEN_NIGHT_PRICE = 46000, CHILD_DAY_PRICE = 44000, 
+			CHILD_NIGHT_PRICE = 37000, OLD_DAY_PRICE = 44000, OLD_NIGHT_PRICE = 37000;
 		
-		public static final long FULL_DIGIT = 10000000000000L;
-		public static final long FULL_DIGIT_MIN = 10000000000L;
-		public static final int SEVEN_DIGIT = 1000000;
+		public static final long FULL_DIGIT = 10000000000000L, FULL_DIGIT_MIN = 10000000000L,
+								SEVEN_DIGIT = 1000000;
 		
-		public static final int TWO_DIGIT = 100;
-		public static final int ONE_DIGIT = 10;
-		public static final int OLD_GENERATION = 1900;
-		public static final int NEW_GENERATION = 2000;
-		public static final int MALE_OLD = 1;
-		public static final int FEMALE_OLD = 2;
-		public static final int MALE_NEW = 3;
-		public static final int FEMALE_NEW = 4;
-		public static final int BEFORE_BIRTH = 2;
-		public static final int AFTER_BIRTH = 1;
+		public static final int TWO_DIGIT = 100,  ONE_DIGIT = 10, 
+								OLD_GENERATION = 1900, NEW_GENERATION = 2000, 
+								MALE_OLD = 1, FEMALE_OLD = 2, MALE_NEW = 3, FEMALE_NEW = 4,
+								BEFORE_BIRTH = 2, AFTER_BIRTH = 1;
 
-		public static final int MIN_BABY = 1;
-		public static final int MIN_CHILD = 3;
-		public static final int MIN_TEEN = 13;
-		public static final int MIN_ADULT = 19;
-		public static final int MAX_CHILD = 12;
-		public static final int MAX_TEEN = 18;
-		public static final int MAX_ADULT = 64;
+		public static final int MIN_BABY = 1, MIN_CHILD = 3, MIN_TEEN = 13, MIN_ADULT = 19,
+								MAX_CHILD = 12, MAX_TEEN = 18, MAX_ADULT = 64;
 
-		public static final int BABY = 1;
-		public static final int CHILD = 2;
-		public static final int TEEN = 3;
-		public static final int ADULT = 4;
-		public static final int OLD = 5;
+		public static final int BABY = 1, CHILD = 2, TEEN = 3, ADULT = 4, OLD = 5;
 		
-		public static final double DISABLE_DISCOUNT_RATE = 0.6;
-		public static final double MERIT_DISCOUNT_RATE = 0.5;
-		public static final double MULTICHILD_DISCOUNT_RATE = 0.8;
-		public static final double PREGNANT_DISCOUNT_RATE = 0.85;
+		public static final double DISABLE_DISCOUNT_RATE = 0.6, MERIT_DISCOUNT_RATE = 0.5,
+									MULTICHILD_DISCOUNT_RATE = 0.8, 
+									PREGNANT_DISCOUNT_RATE = 0.85;
 
-		public static final int MAX_COUNT = 10;
-		public static final int MIN_COUNT = 1;
-
+		public static final int MAX_COUNT = 10, MIN_COUNT = 1;
 }
