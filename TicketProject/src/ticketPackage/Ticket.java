8104d2e802ddc1cc091fa89cd46b80abacf70ade
WSU <woo4091009@naver.com>
2023-04-24 17:37:31 +0900

    변수를 간단하게 표시하고 코드를 짧게 줄임

--- TicketProject/src/ticketPackage/Ticket.java
+++ TicketProject/src/ticketPackage/Ticket.java
@@ -3,58 +3,37 @@ package ticketPackage;
 import java.util.Scanner;
 
 public class Ticket {
-	
+
 	public static void main(String[] args) {
 		inputClass ipc = new inputClass();
 		RunTicketClass rtc = new RunTicketClass();
 		OutputClass opc = new OutputClass();
-		
-		long customerIDNumber = 0;
-		int ticketSelect = 0, orderCount = 0, discountSelect = 0;
-		int age = 0;
-		int continueSelect;
-		int processIntegerationResult;
-		int[][] orderList = new int[100][5];
-		
+		AgeCalc age = new AgeCalc();
 		Scanner scanner = new Scanner(System.in);
-		int cnt = 0;
-		int totalPrice = 0;
-		int isExit;
+
+		long customerIDNumber = 0;
+		int ticketSelect = 0, orderCount = 0, discountSelect = 0, agegroup, totalPrice = 0, calcPrice, Discount;
+		int continueSelect, totalResultPrice = 0, isExit = 0;
+
 		do {
-			while (true) {
-				
-				ipc.inputData();
-				customerIDNumber = inputClass.getCustomerIDNumber();
-				ticketSelect = inputClass.getTicketSelect();
-				discountSelect = inputClass.getDiscountSelect();
-				orderCount = inputClass.getOrderCount();
-				
-				processIntegerationResult = rtc.processIntegration(customerIDNumber, 
-																ticketSelect,
-																discountSelect, orderCount,
-																age);
-				age = rtc.calcAge(customerIDNumber);
-				
-				opc.pricePrint(processIntegerationResult);
-				
-				totalPrice += processIntegerationResult;
-				
-				orderList = rtc.saveOrderList(ticketSelect, age, orderCount, processIntegerationResult, discountSelect,
-									orderList, cnt);	
+			do {
+				ticketSelect = ipc.inputTicketSelect();
+				customerIDNumber = ipc.inputCustomerIDNumber();
+				orderCount = ipc.inputOrderCount();
+				discountSelect = ipc.inputDiscountSelect();
 
-				continueSelect = opc.orderContinue();
-				cnt++;
+				agegroup = age.calcAgeGroup(age.calcAge(customerIDNumber));
+				calcPrice = rtc.calcPriceProcess(agegroup, ticketSelect);
+				Discount = rtc.calcDiscount(calcPrice, discountSelect);
+				totalPrice = rtc.calcPriceResult(Discount, orderCount);
+				rtc.inputData(ticketSelect, customerIDNumber, orderCount, discountSelect, agegroup, totalPrice);
+				continueSelect = ipc.orderContinue();
+				totalResultPrice += totalPrice;
+			} while (continueSelect == 1);
+			opc.orderPrint(totalResultPrice);
 
-				if (continueSelect == 2) {
-					
-					break;	
-				}
-			}
-			opc.orderPrint(totalPrice, orderList, totalPrice, cnt);
-
-			System.out.printf("계속 진행(1: 새로운 주문, 2: 프로그램 종료): ");
+			System.out.println("계속 진행(1: 새로운 주문, 2: 프로그램 종료): ");
 			isExit = scanner.nextInt();
-			totalPrice = 0;
 
 		} while (isExit == 1);
 	}
