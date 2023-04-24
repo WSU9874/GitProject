0f23da216ec30c899b13ea4a2ae25efa53cb8d34
WSU <woo4091009@naver.com>
2023-04-24 17:33:48 +0900

    출력 이외의 코드를 삭제 또는 이동

--- TicketProject/src/ticketPackage/OutputClass.java
+++ TicketProject/src/ticketPackage/OutputClass.java
@@ -3,47 +3,64 @@ package ticketPackage;
 import java.util.Scanner;
 
 public class OutputClass {
-	inputClass ipc = new inputClass();
-	RunTicketClass rtc = new RunTicketClass();
 	Scanner scanner = new Scanner(System.in);
+
+	public void PrintinputTicketSelect() {
+		System.out.println("권종을 선택하세요.\n1. 주간권\n2. 야간권\n");
+	}
+
+	public void PrintinputCustomerIDNumber() {
+		System.out.println("주민등록번호를 입력하세요.(13자리)\n");
+	}
+
+	public void PrintinputOrderCount() {
+		System.out.println("몇개를 주문하시겠습니까(최대 10개)\n");
+	}
+
+	public void PrintinputDiscountSelect() {
+		System.out.println("우대사항을 선택하세요.\n1. 없음\n2. " + "장애인\n3. 국가유공자\n4. 다자녀\n5. 임산부\n");
+	}
+
+	public void errorMessagePrint() {
+		System.out.printf("잘못 입력하셨습니다.\n");
+	}
 
 	public void pricePrint(int priceResult) {
 		System.out.printf("가격은 %d 원 입니다.\n", priceResult);
 		System.out.printf("감사합니다.\n\n");
 	}
-	
-	public void orderPrint(int priceResult, int[][] orderList, int totalPrice, int cnt) {
+
+	public void PrintOrderContinue() {
+
+		System.out.printf("계속 발권 하시겠습니까?\n");
+		System.out.printf("1. 티켓 발권\n");
+		System.out.printf("2. 종료\n");
+	}
+
+	public void orderPrint(int totalPrice1) {
 		System.out.printf("티켓 발권을 종료합니다. 감사합니다.\n");
 		System.out.printf("\n");
 		System.out.printf("===============에버랜드===============\n");
-		
-		for (int i = 0; i < cnt; i++) {
-			switch (orderList[i][0]) {
-			case 1:
-				System.out.printf("주간권  ");
-				break;
-			case 2:
-				System.out.printf("야간권  ");
-				break;
-			default:
-				break;
-			}
+		for (OrderList order : RunTicketClass.data) {
+			if (order.getTicketSelect() == 1)
+				System.out.print("주간권 ");
+			else
+				System.out.print("야간권 ");
 
-			if (rtc.calcAgeGroup(orderList[i][1]) == TicketConstValueClass.BABY) {
-				System.out.printf("유아 ");
-			} else if (rtc.calcAgeGroup(orderList[i][1]) == TicketConstValueClass.CHILD) {
-				System.out.printf("어린이 ");
-			} else if (rtc.calcAgeGroup(orderList[i][1]) == TicketConstValueClass.TEEN) {
-				System.out.printf("청소년 ");
-			} else if (rtc.calcAgeGroup(orderList[i][1]) == TicketConstValueClass.ADULT) {
-				System.out.printf("어른 ");
-			} else {
-				System.out.printf("노인 ");
-			}
-			System.out.printf("X %3d", orderList[i][2]);
-			System.out.printf("%8d원    ", orderList[i][3]);
+			if (order.getAgegroup() == 1)
+				System.out.println("유아   ");
+			else if (order.getAgegroup() == 2)
+				System.out.print("소인   ");
+			else if (order.getAgegroup() == 3)
+				System.out.print("청소년   ");
+			else if (order.getAgegroup() == 4)
+				System.out.print("대인   ");
+			else if (order.getAgegroup() == 5)
+				System.out.print("노인   ");
 
-			switch (orderList[i][4]) {
+			System.out.printf("x %d\t%d원  \t", order.getOrderCount(), order.getTotalPrice());
+
+			switch (order.getDiscountSelect()) {
 			case 1:
 				System.out.printf("*우대적용 없음\n");
 				break;
@@ -60,37 +77,13 @@ public class OutputClass {
 				System.out.printf("*임산부 우대적용\n");
 				break;
 			default:
-				break;
+
 			}
 		}
-		
 		System.out.printf("\n");
-		System.out.printf("입장료 총액은 %d원 입니다.\n", totalPrice);
+		System.out.printf("입장료 총액은 %d원 입니다.\n", totalPrice1);
 		System.out.printf("===================================================\n");
 		System.out.printf("\n");
-}
-	
-	public int orderContinue() {
 
-		int continueSelect = 0;
-		
-		System.out.printf("계속 발권 하시겠습니까?\n");
-		System.out.printf("1. 티켓 발권\n");
-		System.out.printf("2. 종료\n");
-		while (true) {
-			continueSelect = scanner.nextInt();
-			if (continueSelect == 1 || continueSelect == 2) {
-				break;
-			}
-			else {
-				errorMessagePrint();
-			}
-		}
-		return continueSelect;
 	}
-
-	public static void errorMessagePrint() {
-		System.out.printf("잘못 입력하셨습니다.\n");
-	}
-
 }
